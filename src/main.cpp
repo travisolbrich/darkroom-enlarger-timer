231ff0c9fec5cf9e42e40f54ecffe73ed0bd5563
Travis Olbrich <travis@olbrich.io>
2024-04-24 18:13:11 -0500

    Working on cleaning up user system

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 // based on https://www.tmax100.com/photo/pdf/fstoptiming.pdf
 
@@ -15,8 +16,44 @@ const int ENTER_BUTTON = 5;
 
 double baseExposure = 8;
 
+struct Interval {
+  int interval;
+  const char* label;
+};
+
+Interval intervals[] = {
+  {12, "1"},
+  {4, "1/3"},
+  {2, "1/6"},
+  {1, "1/12"}
+};
+
 void testStrip();
-int getStripCount();
+int getStripCount(int strips);
+double getBaseTime(int stripCount);
+Interval getIntervals(int stripCount, double baseTime);
+
+int handleIncrementButtonPress(int pin, int increment, int current) {
+  if (digitalRead(pin) == LOW) {
+    current += increment;
+    delay(200);
+  }
+
+  return current;
+}
+
+bool checkExitCondition(int pin) {
+  if (digitalRead(pin) == LOW) {
+      delay(200);
+      return true;
+
+  }
+  return false;
+}
+
+bool noButtonsPressed() {
+  return digitalRead(UP_BUTTON) == HIGH && digitalRead(DOWN_BUTTON) == HIGH && digitalRead(ENTER_BUTTON) == HIGH;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -57,15 +94,71 @@ double getTime(int step) {
   return time;
 }
 
+void printTestStripInfoLine(int strips) {
+  lcd.setCursor(0, 3);
+  lcd.print("#");
+  lcd.print(strips);
+}
+
+void printTestStripInfoLine(int strips, double time) {
+  printTestStripInfoLine(strips);
+  lcd.print(" ");
+  lcd.print(String(time, 1));
+  lcd.print("s ");
+}
+
+void printTestStripInfoLine(int strips, double time, Interval interval) {
+  printTestStripInfoLine(strips, time);
+  lcd.print("+");
+  lcd.print(interval.label);
+}
+
 void testStrip() {
-  int strips = getStripCount();
-  
+  // int preset = EEPROM.read(0);
+  int preset = 8;
+
+  int strips = getStripCount(preset);
+
+  if (strips != EEPROM.read(0)) {
+    EEPROM.write(0, strips);
+  }
+
+  double baseTime = getBaseTime(strips);
+  Interval interval = getIntervals(strips, baseTime);
+
+  lcd.clear();
+
+  printTestStripInfoLine(strips, baseTime, interval);
+}
+
+int getStripCount(int strips) {
+  lcd.clear();
+  lcd.print("# test strips?");
+
+  bool exit = false;
+
+  while (!exit) {
+    lcd.setCursor(0, 2);
+    lcd.print(strips);
+    lcd.print(" strips        ");
+
+    while (noButtonsPressed()) {
+      delay(10);
+    }
+
+    strips = handleIncrementButtonPress(UP_BUTTON, 1, strips);
+    strips = handleIncrementButtonPress(DOWN_BUTTON, -1, strips);
+    exit = checkExitCondition(ENTER_BUTTON);
+  }
+
+  return strips;
+}
+
+double getBaseTime(int stripCount) {
   lcd.clear();
   lcd.print("Base exposure time?");
-  lcd.setCursor(0, 3);
-  lcd.print(strips);
-  lcd.print(" strips");
-
+  
+  printTestStripInfoLine(stripCount);
 
   bool exit = false;
   int step = 0;
@@ -75,58 +168,48 @@ void testStrip() {
     lcd.print(String(getTime(step), 1));
     lcd.print(" seconds        ");
 
-    while (digitalRead(UP_BUTTON) == HIGH && digitalRead(DOWN_BUTTON) == HIGH && digitalRead(ENTER_BUTTON) == HIGH) {
+    while (noButtonsPressed()) {
       delay(10);
     }
 
-    if (digitalRead(UP_BUTTON) == LOW && step < 100) {
-      step+=1;
-      delay(200);
-    }
-
-    if (digitalRead(DOWN_BUTTON) == LOW && step > 0) {
-      step-=1;
-      delay(200);
-    }
-
-    if (digitalRead(ENTER_BUTTON) == LOW) {
-      exit = true;
-      delay(200);
-    }
+    step = handleIncrementButtonPress(UP_BUTTON, 1, step);
+    step = handleIncrementButtonPress(DOWN_BUTTON, -1, step);
+    exit = checkExitCondition(ENTER_BUTTON);
   }
+
+  return getTime(step);
 }
 
-int getStripCount() {
-  lcd.clear();
-  lcd.print("# test strips?");
+Interval getIntervals(int stripCount, double baseTime){
+  int intervalIndex = 1;
 
-  int strips = 5;
+  lcd.clear();
+  lcd.print("Step size?");
+  lcd.setCursor(0, 2);
+  lcd.print(intervals[intervalIndex].label);
+  lcd.print(" step");
+
+  printTestStripInfoLine(stripCount, baseTime);
+
   bool exit = false;
 
   while (!exit) {
     lcd.setCursor(0, 2);
-    lcd.print(strips);
-    lcd.print(" strips        ");
+    lcd.print(intervals[intervalIndex].label);
+    lcd.print(" steps        ");
 
-    while (digitalRead(UP_BUTTON) == HIGH && digitalRead(DOWN_BUTTON) == HIGH && digitalRead(ENTER_BUTTON) == HIGH) {
+    while (noButtonsPressed()) {
       delay(10);
     }
 
-    if (digitalRead(UP_BUTTON) == LOW && strips < 100) {
-      strips++;
-      delay(200);
-    }
+    intervalIndex = handleIncrementButtonPress(UP_BUTTON, 1, intervalIndex);
+    intervalIndex = handleIncrementButtonPress(DOWN_BUTTON, -1, intervalIndex);
+    
+    // Restrict to valid indicies
+    intervalIndex = max(0, min(3, intervalIndex));
 
-    if (digitalRead(DOWN_BUTTON) == LOW && strips > 1) {
-      strips--;
-      delay(200);
-    }
-
-    if (digitalRead(ENTER_BUTTON) == LOW) {
-      exit = true;
-      delay(200);
-    }
+    exit = checkExitCondition(ENTER_BUTTON);
   }
 
-  return strips;
+  return intervals[intervalIndex];
 }
